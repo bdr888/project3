06992edaac76d7639c8f8b2fd34c89bfefcbf530
kevinbloomquist <rkevinbloom@gmail.com>
2017-01-07 14:07:44 -0700

    dbSetup and models built out. Adds sample data (note comments about intentional omissions) to seed.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize');
 
 
 //figure out url for heroku
-var sequelize = new Sequelize('postgres://rkevinbloomquist@localhost:5432/project3');
+var sequelize = new Sequelize('postgres://rkevinbloomquist@localhost:5432/filmschool');
 
 //Brings in Sequelize and sequelize note: Caps/syntax
 module.exports.Sequelize = Sequelize;
@@ -13,7 +13,21 @@ module.exports.sequelize = sequelize;
 var Board = sequelize.import("./board");
 var Movie = sequelize.import("./movie");
 var User = seqelize.import("./user");
-//note: dash in file name now camel case
-var MovieBoard = sequelize.import("./movie-board");
 
-//stopped here finish adding relationships
+//not sure if below will autogenerate through relationship declaration
+//note: dash in file name now camel case
+// var MovieBoard = sequelize.import("./movie-board");
+
+
+Board.belongsTo(User);
+User.hasMany(Board);
+
+Movie.belongsToMany(Board, {through:MovieBoard});
+Board.belongsToMany(Movie, {through:MovieBoard});
+//stopped here:
+
+Module.exports.models = {
+	User: User,
+	Board: Board,
+	Movie: Movie
+};
