c55b3f96891c4e124bdcb4858132959c9b0f0e4a
briandridge <briandridge@gmail.com>
2017-01-08 13:20:55 -0700

    creates controller files, sets up board routes and controller

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,42 @@
 console.log("routes.js hello world");
 
+//// Express ////
 var express = require('express');
 var router = express.Router();
 
-var boardsController = require('../controllers/boardsController.js');
+//// Controllers ////
+// var userController = require('../controllers/userController');
+var boardController = require('../controllers/boardController.js');
+// var movieController = require('../controllers/movieController.js');
+// var movieBoardController = require('./controllers/movieBoardController.js');
 
-router.route('/', boardsController)
-	.get(boardsController.index);
 
+//// User routes ////
+
+//// Board routes ////
+
+// index
+router.get('/api/board', boardController.index);
+
+// create
+router.post('/api/board', boardController.create);
+
+// show
+router.get('/api/board/:id', boardController.show);
+
+// update
+router.put('/api/board/:id', boardController.update);
+
+// destroy
+router.delete('/api/board/:id', boardController.destroy);
+
+
+
+//// Movie routes ////
+
+//// MovieBoard routes ////
+
+
+
+//// Export ////
 module.exports = router;
